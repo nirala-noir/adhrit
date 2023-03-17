31732030a63a72fea767bd3b84291e8c3bd368b9
Raman Kumar Nirala <Raman.Nirala@aclmobile.onmicrosoft.com>
2023-03-17 12:08:42 +0530

    pushing springboot

---
+++ src/main/java/com/nirala/adhrit/AdhritApplication.java
@@ -0,0 +1,13 @@
+package com.nirala.adhrit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirala/adhrit/AdhritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirala.adhrit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
